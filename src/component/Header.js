108c85c1d9ba362b357ba44c281d779a2ecb72d7
vimal8533 <v1i2m3a4l@gmail.com>
2023-08-18 11:21:10 +0530

    latest update

--- src/component/Header.js
+++ src/component/Header.js
@@ -1,19 +1,43 @@
-import React from 'react'
+import React,{useState} from 'react'
 import './style.css'
 import Linkcompo from './linkcompo'
+import menu from "../logo/menu (1).png"
+import Close from "../logo/close.png"
 
 function Header() {
+  const [count, setCount] = useState(1);
   return (
     <>
-    <div className='the'>
-    <div className="text"> <span className="rotate">The</span>Siren</div>
-    </div>
+      <div className='the'>
+      <div className="text"> <span className="rotate">The</span>Siren</div>
+      <div className='humber' onClick={()=>{
+       if (count % 2 === 0) {
+        setCount(count + 1);
+        console.log("before", count);
+
+      } else {
+        setCount(count - 1);
+
+        console.log("after", count);
+      }
+      }} >
     
+     {
+       count === 0 ? <img src={Close} alt='not found' className='hummenu'/>:<img src={menu} alt='not found' className='hummenu'/>
+     }
+   
+      
+      </div>
     
-<div className='header'>
+        </div>
     
-<Linkcompo/>
-    </div>
+        {
+      count === 0?<div className='header'><Linkcompo/></div>:""
+    }
+      {/* <div className='header'>
+    
+      <Linkcompo/>
+      </div> */}
     
     </>
     
